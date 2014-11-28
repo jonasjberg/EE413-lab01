2f8ace7b9779aae7b4fa5f71b282ca8900b73dbf
Jonas Sjöberg <jomeganas@gmail.com>
2014-11-28 17:56:46 +0100

    Fix render script and abstract.
    Still messed up though.

--- render.sh
+++ render.sh
@@ -4,5 +4,10 @@
 # Script for converting markdown formatted ASCII files to pdf using pandoc.
 # Written in 2014 by Jonas Sjöberg for PRIVATE USE.
 
-pandoc lab-01.md metadata.yaml -r markdown+simple_tables+table_captions+yaml_metadata_block -s -S \
---latex-engine=xelatex --number-sections --toc -o lab-01.pdf
+INPUT="lab-01.md"
+OUTPUT="lab-01.pdf"
+TEMPLATE="./labb.latex"
+
+echo "running pandoc .."
+pandoc "$INPUT" metadata.yaml --template="$TEMPLATE" -r markdown+simple_tables+table_captions+yaml_metadata_block -s -S \
+--latex-engine=xelatex --number-sections --toc -o "$OUTPUT"
